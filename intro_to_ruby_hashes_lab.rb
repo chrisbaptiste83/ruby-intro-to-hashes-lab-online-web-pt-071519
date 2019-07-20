a32a4d9c5502e26ac92e6eb605716d51daf55540
Christopher Baptiste <chrisbaptiste83@gmail.com>
2019-07-20 17:10:04 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,11 +27,20 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-		monopoly =  {
+monopoly =  {
   :railroads=>
-  {:pieces=>4,
+  {
+    :pieces=>4,
+    :rent_in_dollars=>
+    {
+      :one_piece_owned=>25,
+      :two_pieces_owned=>50,
+      :three_pieces_owned=>100,
+      :four_pieces_owned=>200
+    },
     :names=>
-    {:reading_railroad=>
+    {
+      :reading_railroad=>
       {
         "mortgage_value"=>"$100"
       },
@@ -46,15 +55,7 @@ def monopoly_with_third_tier
       :shortline=>
       {
         "mortgage_value"=>"$800"
-      },
-      
-    :rent_in_dollars=>
-    {
-      :one_piece_owned=>25,
-      :two_pieces_owned=>50,
-      :three_pieces_owned=>100,
-      :four_pieces_owned=>200}
-      
+      }
     }
   }
 }
